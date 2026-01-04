18c5906d426a42a59d17d10c524f36cd7a3767a3
WeltAine <caiwenze2@gmail.com>
2026-01-04 23:44:33 +0800

    射线投射器

---
+++ Assets/Scripts/PlayerController/RaycastSensor.cs
@@ -0,0 +1,103 @@
+using UnityEngine;
+
+
+namespace AdvancedPlayerController 
+{
+    public class RaycastSensor
+    {
+        //方向枚举
+        public enum CastDirection
+        {
+            Forward,
+            Up,
+            Right,
+            Backward,
+            Down,
+            Left
+        }
+
+        Transform transform;
+
+        Vector3 origin = Vector3.zero;
+        CastDirection castDirection;
+        public float casrLength = 1f;
+
+        public LayerMask layerMask = 255;
+
+        RaycastHit hitInformation;
+
+
+        public RaycastSensor(Transform transform)
+        {
+            this.transform = transform;
+            castDirection = CastDirection.Forward;
+        }
+
+        public void Cast()
+        {
+            Vector3 worldOrigin = transform.TransformPoint(origin);
+            Vector3 worldCastDirection = GetCastDirection();
+
+            Ray ray = new Ray(worldOrigin, worldCastDirection);
+            Physics.Raycast(ray, out hitInformation, casrLength, layerMask, QueryTriggerInteraction.Ignore);
+        }
+
+
+        public void SetCastOrigin(Vector3 pos)
+        {
+            origin = transform.InverseTransformPoint(pos);
+        }
+
+        public Vector3 GetCastDirection()
+        {
+            return castDirection switch {
+                CastDirection.Forward => transform.forward,
+                CastDirection.Up => transform.up,
+                CastDirection.Right => transform.right,
+
+                CastDirection.Backward => -transform.forward,
+                CastDirection.Down => -transform.up,
+                CastDirection.Left => -transform.right
+            };
+        }
+
+        public void SetCastDirection(CastDirection direction)
+        {
+            castDirection = direction;
+        }
+
+
+
+        public bool HasDetectedHit()
+        {
+            return hitInformation.collider != null;
+        }
+
+        public Transform GetTransform()
+        {
+            return hitInformation.transform;
+        }
+
+        public Vector3 GetNormal()
+        {
+            return hitInformation.normal;
+        }
+
+        public Vector3 GetPosition()
+        {
+            return hitInformation.point;
+        }
+
+        public float GetDistance()
+        {
+            return hitInformation.distance;
+        }
+
+        public Collider GetCollider()
+        {
+            return hitInformation.collider;
+        }
+    }
+
+}
+
